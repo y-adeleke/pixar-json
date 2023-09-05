9f3b33fdd85568663cb74d4991c6c07f93db372b
Goldbird-code <adelekeyusuf29@gmail.com>
2023-09-05 00:15:46 -0400

    completed

---
+++ README.md
@@ -0,0 +1,52 @@
+# Pixar JSON Project
+
+This academic assignment focuses on creating a web-based application that allows users to display movie information and posters from a JSON file. The JSON file (`pixar.json`) contains data about Pixar movies, including their titles, directors, release dates, IMDb ratings, and poster URLs. The user can select this JSON file, and the associated images will be shown on the web page.
+
+## Application Features
+
+### File Selection
+
+- Use the `Choose File` button to select the `pixar.json` file from your local machine.
+
+### Displaying Movie Posters
+
+- Upon selecting the JSON file, the application will display the movie posters on the web page.
+
+### Filtering Movies
+
+- You can filter movies based on the following criteria:
+  - **Release Year**: Select a specific year or choose "All Years" to display all movies.
+  - **Director**: Select a specific director or choose "All Directors" to display all movies.
+  - **Order**: Choose between "Ascending" and "Descending" to change the order of movie display.
+
+## Usage
+
+1. **File Selection**:
+
+   - Click the `Choose File` button and select the `pixar.json` file.
+
+2. **Filter Movies**:
+
+   - Use the filters (`Year`, `Director`, and `Order`) to narrow down the displayed movies.
+
+3. **Reset Filters**:
+   - To reset filters, simply select the default options from each filter dropdown.
+
+## Technologies Used
+
+- HTML
+- JavaScript
+- CSS
+
+## Screenshots
+
+![Pixar](pixar-image.png)
+
+## Link to the website
+
+- https://pixar-movie-loader.netlify.app/
+
+## Acknowledgments
+
+- This project was created as part of an academic assignment to demonstrate web development skills.
+- Data source: The movie data is based on Pixar movies.
---
+++ script.js
@@ -0,0 +1,96 @@
+let fileButton = document.getElementById("moviefile");
+let yearsFilter = document.getElementById("movie-year");
+let directorFilter = document.getElementById("movie-director");
+let orderFilter = document.getElementById("movie-order");
+let moviePoster = document.getElementById("movie-posters");
+
+class Movie {
+  constructor(movie) {
+    let { title, director, releaseDate, imdbRating, posterUrl } = movie;
+    this.title = title;
+    this.director = director;
+    this.releaseYear = releaseDate.split(" ")[2];
+    this.imdbRating = imdbRating;
+    this.posterUrl = posterUrl;
+  }
+  MovieGenerator = () => {
+    let imgcontainer = document.createElement("div");
+    imgcontainer.className = "movie";
+    moviePoster.appendChild(imgcontainer);
+    let imgelement = document.createElement("img");
+    imgelement.src = "./images/" + this.posterUrl;
+    imgcontainer.appendChild(imgelement);
+  };
+}
+
+let moviesobject = [];
+
+///////////////////////////////////////////////////////////
+fileButton.addEventListener("change", function (e) {
+  // Your Code Here
+  let reader = new FileReader();
+  reader.onload = () => {
+    let moviesData = JSON.parse(reader.result);
+    moviePoster.innerHTML = "";
+    moviesData.movies.map((movie) => {
+      moviesobject.push(new Movie(movie));
+    });
+    moviesobject.map((movie) => movie.MovieGenerator());
+  };
+  reader.readAsText(e.target.files[0]);
+});
+
+///////////////////////////////////////////////////////////
+yearsFilter.addEventListener("change", function () {
+  directorFilter.selectedIndex = 0;
+  orderFilter.selectedIndex = 0;
+  // Your Code Here
+  moviePoster.innerHTML = "";
+  let moviesfound = 0;
+
+  if (yearsFilter.value == "All Years") {
+    moviesobject.map((movie) => movie.MovieGenerator());
+    moviesfound++;
+  } else {
+    moviesobject.map((movie) => {
+      if (movie.releaseYear >= yearsFilter.value) {
+        movie.MovieGenerator();
+        moviesfound++;
+      }
+    });
+  }
+  if (moviesfound == 0) console.log("no movies found!");
+});
+
+///////////////////////////////////////////////////////////
+directorFilter.addEventListener("change", function () {
+  yearsFilter.selectedIndex = 0;
+  orderFilter.selectedIndex = 0;
+  // Your Code Here
+  moviePoster.innerHTML = "";
+  let moviesfound = 0;
+  if (directorFilter.value == "All Directors") {
+    moviesobject.map((movie) => movie.MovieGenerator());
+    moviesfound++;
+  } else {
+    moviesobject.map((movie) => {
+      if (movie.director == directorFilter.value) {
+        movie.MovieGenerator();
+        moviesfound++;
+      }
+    });
+  }
+  if (moviesfound == 0) console.log("no movies found!");
+});
+
+///////////////////////////////////////////////////////////
+orderFilter.addEventListener("change", function () {
+  directorFilter.selectedIndex = 0;
+  yearsFilter.selectedIndex = 0;
+  // Your Code Here
+  moviePoster.innerHTML = "";
+  if (orderFilter.value == "Descending") {
+    let reversedMovies = [...moviesobject].reverse();
+    reversedMovies.map((movie) => movie.MovieGenerator());
+  } else moviesobject.map((movie) => movie.MovieGenerator());
+});
